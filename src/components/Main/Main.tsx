0fbc74bd742547b4fb14359c9c12e49e1c7d6a6e
Vladislav Savosko <savosko2009@yandex.ru>
2020-02-17 22:45:10 +0300

    feat: 🎸 implemented icons and their switching in tab-bar

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const Page = styled.div`
+  flex-grow: 1;
+`;
+
 export const Main: React.FC = () => {
   return (
-    <>
+    <Page>
       <p>Main Page</p>
-    </>
+    </Page>
   );
 };
