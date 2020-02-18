f0373eece32eda9a449c84373155c413c6899990
Vladislav Savosko <savosko2009@yandex.ru>
2020-02-18 23:05:35 +0300

    feat: 🎸 created markup for Main component

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -5,13 +5,14 @@ import styled from 'styled-components';
 const Header = styled.div`
   display: flex;
   height: 44px;
-  border-bottom: 2px solid #f3f3f3;
+  border-bottom: 1px solid #f3f3f3;
   background-color: #f9f9f9;
   justify-content: center;
   align-items: center;
 `;
 const Title = styled.p`
   font-family: SFProTextSemibold, Fallback, sans-serif;
+  font-size: 17px;
 `;
 
 export const HeaderBar: React.FC = () => {
