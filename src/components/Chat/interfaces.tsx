b388b61114db81374050368cf7726f888b947c39
Vladislav Savosko <savosko2009@yandex.ru>
2020-03-18 22:56:43 +0300

    feat: 🎸 implemented logic of Chat component & Message component

--- src/components/Chat/interfaces.tsx
+++ src/components/Chat/interfaces.tsx
@@ -2,6 +2,12 @@ export interface IProps {
   socket: SocketIOClient.Socket;
 }
 
+export interface IMessage {
+  text: string;
+  sendingDate: string;
+  sendingTime: string;
+}
+
 export interface IEventInfo {
   target: HTMLInputElement;
   keyCode: number;
--- src/mockData.js
+++ src/mockData.js
@@ -108,3 +108,101 @@ export const lessonsData = [
     },
   ],
 ];
+export const messageData = [
+  {
+    user: 'vssavosko',
+    text: 'test1',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:39',
+  },
+  {
+    user: 'vssavosko',
+    text: 'hi, world',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:41',
+  },
+  {
+    user: 'vssavosko',
+    text: 'hello world',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'testing',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'check',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'hi, vssavosko',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:45',
+  },
+  {
+    user: 'vssavosko',
+    text: 'yeah right',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: ':)',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'test2',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:45',
+  },
+  {
+    user: 'vssavosko',
+    text: 'yeah right',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'test3',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'omg',
+    sendingDate: '2020-04-15T21:00:00.000Z',
+    sendingTime: '22:45',
+  },
+  {
+    user: 'vssavosko',
+    text: 'hi',
+    sendingDate: '2020-03-17T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: 'test4',
+    sendingDate: '2020-03-17T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: '$$$',
+    sendingDate: '2020-03-17T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+  {
+    user: 'vssavosko',
+    text: ':)',
+    sendingDate: '2020-03-17T21:00:00.000Z',
+    sendingTime: '22:33',
+  },
+];
